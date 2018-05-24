c8d5cfe71548a17fdb550a7f87bcd5b73d42b13f
Md. Ashraf Ullah Bhuiyan <ashrafullah274@gmail.com>
2018-05-24 11:42:44 +0600

    Update app.blade.php

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -78,7 +78,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="{{asset('admin/index.blade.php" class="logo"')}}>
+    <a href="{{ route('admin') }}" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
